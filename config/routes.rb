c8e724ae108e2c33b711f0b8eeb20a603783d25e
dagrunestrand <dagrune.strand@gmail.com>
2019-06-09 06:07:43 +0200

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   get 'static/index'
   get 'static/kontakt'
-  get '/assets/frontPageImage.png'
-  get '/assets/Linje1.svg'
-  get '/assets/imageconcept.jpg'
+  get '/assets/images/frontPageImage.png'
+  get '/assets/images/Linje1.svg'
+  get '/assets/images/imageconcept.jpg'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
